ffdbe1b18f1a55ce8436caea7330b5a6727ea9ac
unknown <awamry@awamry.com>
2018-04-25 01:12:21 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express =  require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -29,6 +29,23 @@ app.get('/todos',(req,res) => {
   });
 });
 
+// GET /todos/1241
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id))
+  {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo)
+    {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+      res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
